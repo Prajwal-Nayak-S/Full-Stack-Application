1baf6545ee593390292d49d24b068c4fa9db952c
Prajwal-Nayak-S <prajwalnayaks772@gmail.com>
2025-05-25 09:07:44 +0530

    Update HeaderComponent.jsx

--- ReactFrontEnd/src/Components/todo/HeaderComponent.jsx
+++ ReactFrontEnd/src/Components/todo/HeaderComponent.jsx
@@ -11,7 +11,7 @@ function HeaderComponent(){
             <div className="container">
                 <div className="row">
                     <nav className="navbar navbar-expand-lg">
-                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.in28minutes.com">in28minutes</a>
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://github.com/Prajwal-Nayak-S">GITPROFILE</a>
                         <div className="collapse navbar-collapse">
                             <ul className="navbar-nav">
                                 <li className="nav-item fs-5">{isAuthenticated&&<Link className="nav-link" to="/welcome/in28minutes">Home</Link>}</li>
